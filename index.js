48943d435477654c3567b8089be4846eee05b235
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-02-20 18:39:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logWshiper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return 'I can\'t hear you!';
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    return 'YES INDEED';
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string === '')
+}
