3acfa612a96a05b5da2616a8c4ff1dbecaf4b9c2
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-02-20 19:06:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function sayHiToGrandma(string) {
 function sayHiToGrandma(string) {
   let output = "I love you, Grandma.";
   if (string === output) {
-    return 'I love you, too.'
+    return 'I love you, too.';
   }
 }
